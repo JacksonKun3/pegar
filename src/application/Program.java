49716a62ec87e5638df3e183c4c6c555f5d47e3c
JacksonKun3 <jacksonluismileski@outlook.com>
2020-07-16 22:28:43 -0300

    comment

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,12 +8,14 @@ import entities.enums.OrderStatus;
 public class Program {
 
 	public static void main(String[] args) {
-		Pedido pedido = new Pedido(1080, new Date(), OrderStatus.AGUARDANDO_PAGAMENTO);
-//vai imprimir o status em forma de string  conforme o nome que dei ali, entao por padra no java
-		// ele vai transformar o enum para string conforme está escrito no enum
-		System.out.println(pedido);
+		Pedido pedido = new Pedido(1080, new Date(), OrderStatus.AGUARDANDO_PAGAMENTO); 
+		/* vai imprimir o Order.Status.AGUARDANDO_PAGAMENTO em forma de string, sendo que era do tipo enum*/
+		System.out.println(pedido);//imprimir o toString
 		
-		OrderStatus os1 = OrderStatus.ENTREGUE;
+		//duas formas para transformar um enum numa string
+		OrderStatus os1 = OrderStatus.ENTREGUE;/*estanciou o objeto os1 do tipo OrderStatus com o 
+		valor Order.Status.ENTREGUE,estanciando eu consigo pegar o valor, no caso pegar a string e transformar
+		ela para enum*/
 		OrderStatus os2 = OrderStatus.valueOf("ENTREGUE");
 		System.out.println(os1);
 		System.out.println(os2);
--- src/entities/enums/OrderStatus.java
+++ src/entities/enums/OrderStatus.java
@@ -1,7 +1,9 @@
 package entities.enums;
 
 public enum OrderStatus {
-	
+											//Enumerações
+	/*Pelo o que entendi sao constantes relacionadas, no caso aqui é tudo relacionado pois sao os proximos passos
+	 * entao para ficar de melhor entendimento, é bom separar em uma classe distinta*/
 	AGUARDANDO_PAGAMENTO,
 	PROCESSANDO,
 	ENVIADO,
